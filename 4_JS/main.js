4e82735ab2ba3daaffde31189e833aa7843e1e0a
Mirko <oknight90@gmail.com>
2016-03-21 03:43:56 +0100

    Finally a somehow visually appealing page

--- 4_JS/main.js
+++ 4_JS/main.js
@@ -1,8 +1,31 @@
 $(document).ready(loadOk);
 
 function loadOk(){
-  var menuItem = $("nav").find("li");
-  menuItem.on("mouseenter mouseleave",function(){
-    $(this).toggleClass("hovered");
+  var menuItems = $("nav").find("li");
+  var menuBar = $("nav").find("ul");
+  var menuBox = $("nav").find(".slideIn");
+
+  menuItems.on("mouseover",function(){
+    menuItems.removeClass("hovered");
+    $(this).addClass("hovered");
+    var myUrl = '../menu_' + $(this).data('pos') + '.html';
+    $.ajax(myUrl, {
+      success: function(response) {
+        menuBox.html(response);
+        menuBox.addClass("hovered");
+      },
+      error: function(request, errorType, errorMessage) {
+        console.log('Error: ' + errorType + ' with message: ' + errorMessage);
+        menuBox.removeClass("hovered");
+      }
+    });
+  });
+
+  menuBox.on("mouseover",function(){
+    $(this).addClass("hovered");
+  });
+  menuBox.on("mouseleave",function(){
+    menuItems.removeClass("hovered");
+    menuBox.removeClass("hovered");
   });
 }
