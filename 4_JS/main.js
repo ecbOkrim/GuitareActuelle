268d8ef2cc605ea5bd9dfc161dd4bd99a17c3175
Mirko <oknight90@gmail.com>
2016-04-10 21:39:07 +0200

    Looking very good
    Menu animation is taking shape

--- 4_JS/main.js
+++ 4_JS/main.js
@@ -1,61 +1,43 @@
+var delayId = null;
+var delayActive = false;
+var menuBase = null;
+
 $(document).ready(loadOk);
 
 function loadOk(){
-  var menuBtn = $("#mainMenu");
-  // var title = $("#titleContainer");
-  // var titleTxt = $("#titleContainer").find(".content");
-  var titLink = $("#titleContainer").find("a");
-  var sideLinks = $("#sideContainer").find("a");
-
-  console.log(location.pathname);
-  if(location.pathname == "/" || location.pathname == "/index.html"){
-    titLink.href = "#";
-  }
-
-  $.each(sideLinks, function(indx,item){
-    var tmp = item.href.substring(item.href.lastIndexOf("/") + 1);
-
-    if(tmp == location.pathname.substring(1)){
-      item.href = "#";
-    }
-  });
-
-  menuBtn.on("mouseover", function(){
+  menuBase = $(".menuBase");
+  menuBase.on("mouseover", function(){
+    menuBase.not(this).each(function(){
+      mOut($(this));
+    });
     mOver($(this));
   });
-  menuBtn.on("mouseleave", function(){
-    mOut($(this));
-  });
-  menuBtn.on("click", function(){
-    mClick($(this));
-  });
 
-/*
-  title.on("mouseover", function(){
-    mOver($(titleTxt));
+  menuBase.on("mouseleave", function(){
+    mOutDelay($(this));
   });
-  title.on("mouseleave", function(){
-    mOut($(titleTxt));
-  });
-  */
 }
 
 function mOver(a){
-  if (!$(a).hasClass("selected")) {
-      $(a).addClass("hovered");
+  if(delayActive){
+    clearTimeout(delayId);
+    delayActive = false;
   }
+  if(!$(a).hasClass("empty")){
+    $(a).find(".title").addClass("active");
+    $(a).find("li").addClass("active");
+  }
+  $(a).find(".titleBorder").addClass("active");
+}
+
+function mOutDelay(a){
+  var me = $(a);
+  delayActive = true;
+  delayId = setTimeout(mOut, 500, me);
 }
 
 function mOut(a){
-  $(a).removeClass("hovered");
-}
-
-function mClick(a){
-  if ($(a).hasClass("selected")) {
-      $(a).removeClass("selected");
-      $(a).addClass("hovered");
-  }else{
-      $(a).removeClass("hovered");
-      $(a).addClass("selected");
-  }
+  $(a).find(".title").removeClass("active");
+  $(a).find(".titleBorder").removeClass("active");
+  $(a).find("li").removeClass("active");
 }
---
+++ 4_JS/main_Bk.js
@@ -0,0 +1,61 @@
+/*
+$(document).ready(loadOk);
+
+function loadOk(){
+  var menuBtn = $("#mainMenu");
+  // var title = $("#titleContainer");
+  // var titleTxt = $("#titleContainer").find(".content");
+  var titLink = $("#titleContainer").find("a");
+  var sideLinks = $("#sideContainer").find("a");
+
+  console.log(location.pathname);
+  if(location.pathname == "/" || location.pathname == "/index.html"){
+    titLink.href = "#";
+  }
+
+  $.each(sideLinks, function(indx,item){
+    var tmp = item.href.substring(item.href.lastIndexOf("/") + 1);
+
+    if(tmp == location.pathname.substring(1)){
+      item.href = "#";
+    }
+  });
+
+  menuBtn.on("mouseover", function(){
+    mOver($(this));
+  });
+  menuBtn.on("mouseleave", function(){
+    mOut($(this));
+  });
+  menuBtn.on("click", function(){
+    mClick($(this));
+  });
+
+  title.on("mouseover", function(){
+    mOver($(titleTxt));
+  });
+  title.on("mouseleave", function(){
+    mOut($(titleTxt));
+  });
+}
+
+function mOver(a){
+  if (!$(a).hasClass("selected")) {
+      $(a).addClass("hovered");
+  }
+}
+
+function mOut(a){
+  $(a).removeClass("hovered");
+}
+
+function mClick(a){
+  if ($(a).hasClass("selected")) {
+      $(a).removeClass("selected");
+      $(a).addClass("hovered");
+  }else{
+      $(a).removeClass("hovered");
+      $(a).addClass("selected");
+  }
+}
+*/
